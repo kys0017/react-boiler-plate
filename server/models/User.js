4eff678d1e3d8446a6f8f741ebf99bf994a070e5
kys0017 <kys0017@gmail.com>
2021-10-22 16:55:15 +0900

    🐛 methods -> statics

--- server/models/User.js
+++ server/models/User.js
@@ -76,7 +76,7 @@ userSchema.methods.generateToken = function (cb) {
 
 };
 
-userSchema.methods.findByToken = function (token, cb) {
+userSchema.statics.findByToken = function (token, cb) {
     var user = this;
 
     // 토큰을 decode 한다.
