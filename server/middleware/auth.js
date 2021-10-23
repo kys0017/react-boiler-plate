2a97966e317ad1267e4bfbd4c582c1a7d386d316
kys0017 <kys0017@gmail.com>
2021-10-23 19:58:34 +0900

    🐛 token null 에러 수정

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,8 +2,9 @@ const {User} = require("../models/User");
 let auth = (req, res, next) => {
     // 인증 처리를 하는 곳
 
-    // 클라이언트 쿠케이서 토큰을 가져온다.
-    let token = req.cookies.x_auth;
+    // 클라이언트 쿠키에서 토큰을 가져온다.
+    // let token = req.cookies.x_auth; // 이건 안먹히네
+    let token = req.cookies['x-auth'];
 
     // 토큰을 복호화 한 후 유저를 찾는다.
     User.findByToken(token, (err, user) => {
@@ -14,10 +15,6 @@ let auth = (req, res, next) => {
         req.user = user;
         next()
     })
-
-    // 유저가 잇으면 인증 ok
-
-    // 유저가 없으면 인증 no
 }
 
 module.exports = {auth}
