8ef7ae07d7e7a2c108603b550fc0e0740d7258fe
kys0017 <kys0017@gmail.com>
2021-10-23 19:57:48 +0900

    📝 landing, login, register 페이지 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import axios from "axios";
+import {withRouter} from "react-router-dom";
 
 function LandingPage(props) {
 
@@ -9,10 +10,32 @@ function LandingPage(props) {
 
     }, []);
 
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(res=> {
+                console.log(res.data)
+                if (res.data.success) {
+                    props.history.push('/login');
+                } else {
+                    alert('로그아웃 하는데 실패했습니다.')
+                }
+            })
+    }
+
 
     return (
-        <div>LandingPage</div>
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}
+        >
+            <h2>시작페이지</h2>
+            
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
+        </div>
     );
 }
 
-export default LandingPage;
+export default withRouter(LandingPage);
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
+import {useDispatch} from "react-redux";
+import {loginUser} from "../../../_actions/user_action";
+import {withRouter} from "react-router-dom";
 
 function LoginPage(props) {
+    const dispatch = useDispatch()
+
+    const [Email, setEmail] = useState('');
+    const [Password, setPassword] = useState('');
+
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value)
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault(); // page refresh 방지
+
+        let body ={
+            email: Email,
+            password: Password
+        };
+
+        dispatch(loginUser(body))
+            .then(res => {
+                if (res.payload.loginSuccess) {
+                    props.history.push('/');
+                } else {
+                    alert('Error')
+                }
+            })
+   }
+
     return (
-        <div>LoginPage</div>
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}
+            onSubmit={onSubmitHandler}
+        >
+            <form style={{
+                display: 'flex', flexDirection: 'column'
+            }}>
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler}/>
+                <br />
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
     );
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
