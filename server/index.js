e3e170a1f2d3e6568bc3f945748687ea661c62b7
kys0017 <kys0017@gmail.com>
2021-10-23 20:01:57 +0900

    👽️ logout api 추가

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/api/hello', (req, res) => {
+    res.send('안녕하세요~');
+})
+
 app.post('/api/users/register', (req, res) => {
     // 회원가입 할 때 필요한 정보들을 client 에서 가져오면
     // 그것들을 디비에 넣어준다
@@ -82,6 +86,15 @@ app.get('/api/users/auth', auth, (req, res) => {
         image: req.user.image
     })
 
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token: ''}, (err, user) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).send({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
